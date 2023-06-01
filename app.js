3aecfbccd6a12492ee6ca3acdfa7f4034e2c9b0f
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2023-06-01 06:59:47 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+const iconMenu = document.querySelector('.menu__icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu__body');
+  iconMenu.addEventListener("click", function (e) {
+    document.body.classList.toggle('_lock')
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+} 
+
+
+
 let tg = window.Telegram.WebApp;
 
 tg.expand();
