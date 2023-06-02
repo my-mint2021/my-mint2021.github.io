3b67d7f6000b1681ed0a87e6f926254a15c23e57
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2023-06-02 20:37:35 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
       let tg = window.Telegram.WebApp;
       let buy = document.getElementById("buy");
       let order = document.getElementById("order");
+      tg.expand();
 
       buy.addEventListener("click", () => {
         document.getElementById("main").style.display = "none";
@@ -9,5 +10,29 @@
       });
 
       order.addEventListener("click", () => {
+      document.getElementById("error").innerText = '';
+        let name = document.getElementById("user_name").value;
+        let email = document.getElementById("user_email").value;
+        let phone = document.getElementById("user_phone").value;
+        if(name.lenght < 5) {
+            document.getElementById("error").innerText = 'Ошибка в имени';
+            return;
+        }
+        if(email.lenght < 5) {
+            document.getElementById("error").innerText = 'Ошибка в email';
+            return;
+        }
+        if(phone.lenght < 5) {
+            document.getElementById("error").innerText = 'Ошибка в номере телефона';
+            return;
+        }
+
+        let data = {
+            name: name,
+            email: email,
+            phone: phone
+        }
+        tg.sendData(JSON.stringify(data));
+
         tg.close();
-      });
+      });button
