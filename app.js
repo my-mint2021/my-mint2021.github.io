ba27a1c9fd1d60458a3ed0b69b35cffb3ff9f5d1
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2023-06-02 09:09:58 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-var tgLink = document.getElementById('link');
-tgLink.addEventListener('click', function(event) {
+// Получаем ссылку на элемент <a>
+const link = document.getElementById('link');
+
+// Добавляем обработчик события на клик по ссылке
+link.addEventListener('click', (event) => {
+  // Отменяем действие по умолчанию для ссылки
   event.preventDefault();
-  var url = tgLink.getAttribute('href');
-  window.TelegramWebApp.openUrl(url);
+
+  // Получаем адрес ссылки и заменяем http на tg
+  const url = event.target.href.replace('http', 'tg');
+
+  // Открываем ссылку внутри бота
+  window.open(url, '_self');
 });
