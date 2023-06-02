566d589aa3128a94975097ac7009beaa24af2dde
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2023-06-02 15:54:10 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 
       buy.addEventListener("click", () => {
         document.getElementById("main").style.display = "none";
-        document.getElementById("form").style.display = "block";
+        document.getElementById("form").style.display = "flex";
         document.getElementById("user_name").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
       });
 
