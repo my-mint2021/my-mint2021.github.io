ba05a58eadcbe1f0c19b4b116a8b0218aeee47e3
my-mint2021 <81299128+my-mint2021@users.noreply.github.com>
2023-06-22 10:54:36 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,46 +1,28 @@
-      let tg = window.Telegram.WebApp;
-      let buy = document.getElementById("buy");
-      let order = document.getElementById("order");
-      let ata = document.getElementById("ata");
-      tg.expand();
+const buttons = document.querySelectorAll('.btn');
+const divs = document.querySelectorAll('.categ');
+const main = document.querySelector('.main');
+const backButton = document.querySelectorAll('.back');
 
-      ata.addEventListener("click", () => {
-       /* openTelegramLink(url'https://my-mint2021.github.io/cat.html');*/
-        console.log('нажата кнопка');
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('hello');
+        const target = button.dataset.target;
+        divs.forEach(categ => {
+            if (categ.id === target) {
+                categ.style.display = 'block';
+            } else {
+                categ.style.display = 'none';
+            }
+        });
+        main.style.display = 'none';
+    });
+});
+
+backButton.forEach(button => {
+    button.addEventListener('click', () => {
+      main.style.display = 'block';
+      divs.forEach(block => {
+        block.style.display = 'none';
       });
-
-
-
-      buy.addEventListener("click", () => {
-        document.getElementById("main").style.display = "none";
-        document.getElementById("form").style.display = "flex";
-        document.getElementById("user_name").value = tg.initDataUnsafe.user.first_name + " " + tg.initDataUnsafe.user.last_name;
-      });
-
-      order.addEventListener("click", () => {
-      document.getElementById("error").innerText = '';
-        let name = document.getElementById("user_name").value;
-        let email = document.getElementById("user_email").value;
-        let phone = document.getElementById("user_phone").value;
-        if(name.length < 5) {
-            document.getElementById("error").innerText = 'Ошибка в имени';
-            return;
-        }
-        if(email.length < 5) {
-            document.getElementById("error").innerText = 'Ошибка в email';
-            return;
-        }
-        if(phone.length < 5) {
-            document.getElementById("error").innerText = 'Ошибка в номере телефона';
-            return;
-        }
-
-        let data = {
-            name: name,
-            email: email,
-            phone: phone
-        }
-        tg.sendData(JSON.stringify(data));
-
-        tg.close();
-      });
+    });
+  });
